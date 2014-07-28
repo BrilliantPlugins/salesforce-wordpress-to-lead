d71d99207387cf83f786ae7c415aeabc9f247aec
Nick Ciske <nick@thoughtrefinery.com>
2014-07-28 13:47:23 -0500

    Implode lead source if it's an array, change delimiter to semi-colon to match SF style

--- salesforce.php
+++ salesforce.php
@@ -670,12 +670,12 @@ function salesforce_cc_admin($post, $options, $form_id = 1){
 			$label = $options['forms'][$form_id]['inputs'][$name]['label'];
 
 			if( trim( $label ) != '' && $name != 'lead_source' )
-				$message .= stripslashes($label).': '. salesforce_maybe_implode( ',', $value)."\r\n";
+				$message .= stripslashes($label).': '. salesforce_maybe_implode( ';', $value)."\r\n";
 		}
 	}
 
 	if ( $post['lead_source'] ) {
-		$message .= "\r\n".'Lead Source: '.$post['lead_source']."\r\n";
+		$message .= "\r\n".'Lead Source: '.salesforce_maybe_implode( ';', $post['lead_source'] )."\r\n";
 	}
 
 	$emails = array( get_option('admin_email') );
