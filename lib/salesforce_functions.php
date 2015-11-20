fa58b091e71b21c21a7fa3c3dd2d0124e9caa207
Nick Ciske <nick@thoughtrefinery.com>
2015-11-20 10:50:12 -0600

    Breakout v2 and v3 functions to seperate files
    Signed-off-by: Nick Ciske <nick@thoughtrefinery.com>

--- lib/salesforce_functions.php
+++ lib/salesforce_functions.php
@@ -87,12 +87,11 @@ function salesforce_get_form_id_by_post_id( $post_id ){
 
 }
 
-function salesforce_get_form_id( $form_id, $sidebar ){
+function get_salesforce_form_id( $form_id, $sidebar ){
 
-	return 'salesforce_w2l_lead_'.$form_id.str_replace(' ','_',$sidebar);
+	return 'sf_form_salesforce_w2l_lead_' . $form_id . str_replace( ' ', '_', $sidebar) ;
 
 }
-
 function salesforce_get_post_type_slug(){
 	return 'salesforce_w2l_form';
 }
@@ -103,4 +102,144 @@ function salesforce_get_option_name(){
 
 function salesforce_get_meta_key(){
 	return '_salesforce_form_data';
+}
+
+// Add Daddy Analytics JS tracking to all pages
+function salesforce_da_js(  ){
+
+	$options = get_option("salesforce2");
+
+	if( isset( $options['da_token'] ) && $options['da_token'] && isset( $options['da_url'] ) && $options['da_url'] && isset( $options['da_site'] ) && $options['da_site'] ){
+
+		$da_token = $options['da_token'];
+		$da_url = $options['da_url'];
+		$da_site = $options['da_site'];
+
+		echo "\n\n".'<!-- Begin Daddy Analytics code provided by Salesforce to Lead Plugin-->
+		<script src="//cdn.daddyanalytics.com/w2/daddy.js" type="text/javascript"></script>
+		<script type="text/javascript">
+		var da_data = daddy_init(\'{ "da_token" : "'.esc_attr($da_token).'", "da_url" : "'.esc_attr($da_url).'" }\');
+		var clicky_custom = {session: {DaddyAnalytics: da_data}};
+		</script>
+		<script src="//hello.staticstuff.net/w/__stats.js" type="text/javascript"></script>
+		<script type="text/javascript">try{ clicky.init( "'.esc_attr($da_site).'" ); }catch(e){}</script>'."<!-- End Daddy Analytics code provided by Salesforce to Lead Plugin-->\n\n";
+
+	}
+
+}
+
+function salesforce_captcha(){
+	include("lib/captcha/captcha.php");
+	die();
+}
+
+// Widget Class
+if ( ! class_exists( 'Salesforce_WordPress_to_Lead_Widgets' ) ) {
+	require_once('salesforce_widget.class.php');
+	add_action( 'widgets_init', 'salesforce_widget_func' );
+}
+
+function salesforce_widget_func() {
+	register_widget( 'Salesforce_WordPress_to_Lead_Widgets' );
+}
+
+// Procedural Functions
+// TODO: wrap in a class
+
+function salesforce_default_settings() {
+	$options = array();
+	$options['version'] 			= '2.0';
+	$options['successmsg'] 			= __('Success!','salesforce');
+	$options['errormsg'] 			= __('This field is required.','salesforce');
+	$options['emailerrormsg']		= __('The email address you entered is not valid.','salesforce');
+	$options['captchaerrormsg']		= __('The text you entered did not match the image.','salesforce');
+	$options['requiredfieldstext'] 	= __('These fields are required.','salesforce');
+	$options['sferrormsg'] 			= __('Failed to connect to Salesforce.com.','salesforce');
+	$options['submitbutton']	 	= __('Submit','salesforce');
+	$options['subject']	 			= __('Thank you for contacting %BLOG_NAME%','salesforce');
+	$options['showccuser'] 			= true;
+	$options['ccusermsg']			= __('Send me a copy','salesforce');
+	$options['email_sender']		= '';
+	$options['ccadmin']				= false;
+	$options['captcha']				= false;
+
+	$options['da_token']			= '';
+	$options['da_url']				= '';
+	$options['da_site']				= '';
+
+	$options['commentstoleads']    = false;
+	$options['commentsnamefields']  = false;
+
+	$options['usecss']				= true;
+	$options['wpcf7css']			= false;
+	//$options['hide_salesforce_link']= true;
+
+	$options['forms'][1] = Salesforce_Admin::default_form();
+
+	update_option('salesforce2', $options);
+
+	return $options;
+}
+
+function salesforce_back_link($url){
+
+	return '<a href="'.$url.'">&laquo; '.__('Back to configuration page','salesforce').'</a>';
+
+}
+
+/**
+ * Sort input array by $subkey
+ * Taken from: http://php.net/manual/en/function.ksort.php
+ */
+function salesforce_sksort( &$array, $subkey="id", $sort_ascending=false ) {
+
+	if( !is_array( $array ) )
+		return $array;
+
+	$temp_array = array();
+
+    if (count($array))
+        $temp_array[key($array)] = array_shift($array);
+
+    foreach($array as $key => $val){
+        $offset = 0;
+        $found = false;
+        foreach($temp_array as $tmp_key => $tmp_val)
+        {
+            if(!$found and strtolower($val[$subkey]) > strtolower($tmp_val[$subkey])) {
+                $temp_array = array_merge(    (array)array_slice($temp_array,0,$offset),
+                                            array($key => $val),
+                                            array_slice($temp_array,$offset)
+                                          );
+                $found = true;
+            }
+            $offset++;
+        }
+        if(!$found) $temp_array = array_merge($temp_array, array($key => $val));
+    }
+
+    if ($sort_ascending) $array = array_reverse($temp_array);
+
+    else $array = $temp_array;
+}
+
+function salesforce_get_post_data( $index ){
+	if( isset( $_POST[$index] ) ){
+		return $_POST[$index];
+	}else{
+		return false;
+	}
+}
+
+function salesforce_maybe_implode( $delimiter, $data ){
+
+	if( is_array($data) )
+		return trim( implode( $delimiter, $data ) );
+
+	return $data;
+
+}
+
+function salesforce_clean_field( $value ){
+	return trim(strip_tags(stripslashes( $value )));
 }
---
+++ lib/salesforce_v3.php
@@ -0,0 +1,3 @@
+<?php
+// New v3+ form output & submission
+// Uses post id as form id and [salesforce_form id="XX"] shortcode
